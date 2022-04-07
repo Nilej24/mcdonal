fc26bebeee9cb587a287a7efb663972a6fdbbf9c
Nijel Manalo <nmanalo543@gmail.com>
2022-04-07 12:23:46 +0100

    updated styles + some js so that it's clear which tab is selected, I WILL RETURN AT SOME POINT TO TRY OUT BOOTSTRAP CSS

--- src/index.js
+++ src/index.js
@@ -5,9 +5,21 @@ import contact from "./contact.js";
 const homeBtn = document.querySelector(".home");
 const menuBtn = document.querySelector(".menu");
 const contactBtn = document.querySelector(".contact");
+const btns = [homeBtn, menuBtn, contactBtn];
 
 homeBtn.onclick = home;
 menuBtn.onclick = menu;
 contactBtn.onclick = contact;
 
+// changes buttnon border on click to make it look nicer
+btns.forEach(btn => {
+  btn.addEventListener("click", (ev) => styleBtnAsSelected(ev.target));
+});
+
+function styleBtnAsSelected(btn) {
+  btns.forEach(b => {b.style.border = "1px black solid"; b.style.borderLeft = "none";});
+  btn.style.borderBottom = "none";
+}
+
 home();
+styleBtnAsSelected(homeBtn);
